5d7c79c6f30ea240a7bbecc866a3a37c873f986e
Prathameshmagdum <prathameshmagdum2611@gmail.com>
2025-02-19 15:46:35 +0530

    added the api url of swiggy

--- src/Component/Body.js
+++ src/Component/Body.js
@@ -1,10 +1,23 @@
 import React from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () =>{
     const [restaurantList, setRestaurantList] = useState(resList);
+
+    useEffect(()=>{
+        fetchData();
+    },[]);
+
+    const fetchData = async ()=>{
+        const data = await fetch (
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&collection=83639&tags=layout_CCS_Biryani&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
+        );
+        const json = await data.json();
+        console.log(json);
+        // setRestaurantList();
+    }
 
     return (
         <div className="body">
