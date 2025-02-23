26f64a24d15fee070e4b29b78051e9035b45bc95
Prathameshmagdum <prathameshmagdum2611@gmail.com>
2025-02-23 11:54:07 +0530

    ADDED THE SHIMMER BU NOT WORKING

--- src/Component/Body.js
+++ src/Component/Body.js
@@ -2,9 +2,10 @@ import React from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () =>{
-    const [restaurantList, setRestaurantList] = useState(resList);
+    const [restaurantList, setRestaurantList] = useState([]);
 
     useEffect(()=>{
         fetchData();
@@ -15,26 +16,30 @@ const Body = () =>{
             "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.406498&lng=78.47724389999999&collection=83639&tags=layout_CCS_Biryani&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
         );
         const json = await data.json();
-        console.log(json);
+        // console.log(json);
         const restList = json?.data?.cards
             ?.map((card) => card?.card?.card?.info)
             .filter((info) => info);
         setRestaurantList(restList);
-        console.log(restList);
+        // console.log(restList);
+    }
+
+    if(restaurantList.length === 0){
+        return <Shimmer/>
     }
 
     return (
         <div className="body">
             <div className="filter">
                 <button className="filter-btn" onClick={()=>{
-                    const filterRes = restaurantList.filter((res)=>res?.data?.card?.card?.info?.avgRating>=4);
+                    const filterRes = restaurantList.filter((res)=>res?.card?.card?.info?.avgRating>=4);
                     setRestaurantList(filterRes);
                 }}>
                     Filter The Top Rating Restaurant
                 </button>
             </div>
             <div className="res-container">
-                {restaurantList.map((restuarant) => (<RestaurantCard key={restuarant?.data?.card?.card?.info?.id} resData={restuarant}/>))}
+                {restaurantList.map((restuarant) => (<RestaurantCard key={restuarant?.card?.card?.info?.id} resData={restuarant}/>))}
             </div>
         </div>
     )
