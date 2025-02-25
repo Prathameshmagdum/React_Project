9dd8f1791af1015a316a0cd1e96eb481f068af33
Prathameshmagdum <prathameshmagdum2611@gmail.com>
2025-02-25 15:35:22 +0530

    shimmer working

--- src/Component/Body.js
+++ src/Component/Body.js
@@ -24,11 +24,7 @@ const Body = () =>{
         // console.log(restList);
     }
 
-    if(restaurantList.length === 0){
-        return <Shimmer/>
-    }
-
-    return (
+    return restaurantList.length === 0 ? <Shimmer/> : (
         <div className="body">
             <div className="filter">
                 <button className="filter-btn" onClick={()=>{
