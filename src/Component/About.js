5586aff0fae9dcd023f38c8cc70a2b1d7636d769
Prathameshmagdum <prathameshmagdum2611@gmail.com>
2025-03-10 20:37:45 +0530

    added the state variable of class

--- src/Component/About.js
+++ src/Component/About.js
@@ -6,7 +6,6 @@ const About = ()=>  {
         <div>
             <h1>About Us</h1>
             <h2>Im learning the namaste react from akshay saini thank you!</h2>
-            <User name={"Prthamesh Magdum"} location={"Pimpri-Chinchwad"}/>
             <UserClass name={"Prthamesh Magdum (class)"} location={"Pimpri-Chinchwad"}/>
         </div>
     )
--- src/Component/UsserClass.js
+++ src/Component/UsserClass.js
@@ -3,12 +3,27 @@ import React from "react";
 class UserClass extends React.Component{
     constructor(props){
         super(props);
+        this.state={
+            count:0,
+            count1:1,
+        }
     }
     render(){
+        const{name, location} = this.props;
+        const{count,count1}=this.state;
         return(
             <div className="user-card">
-                <h2>Name: {this.props.name}</h2>
-                <h3>Location:{this.props.location}</h3>
+                <h1>Count:{count}</h1>
+                <h1>Count:{count1}</h1>
+                <button onClick={()=>{
+                    this.setState({
+                        count: this.state.count +1,
+                        count1: this.state.count +1,
+                    })
+                }}>Count Increase</button>
+                <h2>Name: {name}</h2>
+                <h3>Location:{location}</h3>
+                
             </div>
         )
     }
