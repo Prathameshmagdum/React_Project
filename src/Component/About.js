9b94b73e4b129ad5190a98a99cc417e455a711c9
Prathameshmagdum <prathameshmagdum2611@gmail.com>
2025-03-10 19:42:41 +0530

    Added the two user and userclass

--- src/Component/About.js
+++ src/Component/About.js
@@ -1,8 +1,13 @@
+import User from "./User";
+import UserClass from "./UsserClass";
+
 const About = ()=>  {
     return (
         <div>
             <h1>About Us</h1>
             <h2>Im learning the namaste react from akshay saini thank you!</h2>
+            <User name={"Prthamesh Magdum"} location={"Pimpri-Chinchwad"}/>
+            <UserClass name={"Prthamesh Magdum (class)"} location={"Pimpri-Chinchwad"}/>
         </div>
     )
 }
---
+++ src/Component/User.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const User = ({name, location}) =>{
+    return (
+        <div className="user-card">
+            <h2>Name: {name}</h2>
+            <h3>Location: {location}</h3>
+        </div>
+    );
+}
+
+export default User;
